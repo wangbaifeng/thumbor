aaeb5c54bbb9d23bd39e9886da3e721526af2d0f
wangbaifeng <50959902@qq.com>
2017-07-06 17:06:05 +0800

    添加CWEB_PATH

--- thumbor/config.py
+++ thumbor/config.py
@@ -202,7 +202,7 @@ Config.define('ENABLE_ETAGS', True, 'Enables automatically generated etags', 'HT
 Config.define('MAX_ID_LENGTH', 32, 'Set maximum id length for images when stored', 'Storage')
 Config.define('GC_INTERVAL', 60, 'Set garbage collection interval in seconds', 'Performance')
 Config.define('LOG_LEVEL', 'debug', 'Set log level', 'Log Level')
-Config.define('GIF2WEB_PATH', None, 'Convert a GIF image to WebP', 'Extensibility')
+Config.define('CWEB_PATH', None, 'Convert a GIF image to WebP', 'Extensibility')
 Config.define('PNG2WEB_PATH', None, 'Convert a GIF image to WebP', 'Extensibility')
 # METRICS OPTIONS
 Config.define('STATSD_HOST', None, 'Host to send statsd instrumentation to', 'Metrics')
